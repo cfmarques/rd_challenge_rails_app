7eff9f14a1576e3f200dbf9b0cba7949dd70fbc7
Carlos Frederico Marques de Lemos <cf.marques@live.com>
2017-11-16 00:32:12 -0200

    Adjusting routes root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'contacts#index'
+
   post '/track', to: 'tracking#track'
+
   resources :contacts, only: [ :index, :show ]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
